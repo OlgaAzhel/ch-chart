b4e5293c11dacd7647c73c9816130fd259e83816
Olga Azhel <olga.azhel@gmail.com>
2023-04-12 13:30:26 -0400

    play scale and play key state added to settings panel

--- src/components/HarmonicaChart/HarmonicaChart.jsx
+++ src/components/HarmonicaChart/HarmonicaChart.jsx
@@ -1,68 +1,157 @@
 import './HarmonicaChart.css'
 import { useState } from "react";
+import * as harmonicaService from '../../utilities/harmonica-service'
 
 
-const baseNotes = [
-    'C','C#/D♭','D','D#/E♭','E','F','F#/G♭','G','G#/A♭','A','A#/B♭','B'
-]
-
-const reorderBaseNotesByKey = (key) => {
-    const index = baseNotes.indexOf(key)
-    const part1 = baseNotes.slice(index)
-    const part2 = baseNotes.slice(0, index)
-    const result = part1.concat(part2)
-    console.log('notes in key of:', key, ' ', result);
-    return result
-}
-
-
-const layoutPattern = [2, 1, 3, 4, 6, 5, 6, 7, 9, 8, 10, 11, 2, 1, 12, 1]
 
 
 export default function HarmonicaChart() {
-    const [scale, setScale] = useState(null)
-    const [playKey, setPlayKey] = useState(null)
-
+    const [scale, setScale] = useState('Major Scale')
+    const [playKey, setPlayKey] = useState('C')
     const [harmonicaKey, setHarmonicaKey] = useState('C')
-    const notes = reorderBaseNotesByKey(harmonicaKey)
+    const [playNotes, setPlayNotes] = useState(harmonicaService.notesFinder(playKey, scale))
+    const notes = harmonicaService.reorderBaseNotesByKey(harmonicaKey)
 
     function harmonicaKeyChange(evt) {
         const newHarmonicaKey = evt.target.value
         setHarmonicaKey(newHarmonicaKey)
     }
+    function playScaleChange(evt) {
+        const newScale = evt.target.value
+        setScale(newScale)
+        setPlayNotes(harmonicaService.notesFinder(playKey, newScale))
+    }
+    function playKeyChange(evt) {
+        const newPlayKey = evt.target.value
+        setPlayKey(newPlayKey)
+        setPlayNotes(harmonicaService.notesFinder(newPlayKey, scale))
+    }
 
-
+    console.log("THIS IS PLAY KEY:",playKey,"THIS IS PLAY SCALE:", harmonicaService.scales[scale])
+    console.log("THESE ARE PLAY NOTES",playNotes)
 
     return <>
         <p>THIS IS YOUR HARMONICA NOTES LAYOUT</p>
+        <section id="settings">
+            <div className="setting">
+            <label>Harmonica key:</label>
+            <select name="harmonica-key"
+                onChange={harmonicaKeyChange}
+                value={harmonicaKey}
 
-        <h5>Harmonica key:</h5>
-        <select name="harmonica-key"
-        onChange={harmonicaKeyChange}
-        value={harmonicaKey}
+            >
+                {
+                    harmonicaService.baseNotes.map((note, idx) => {
+                        return <option key={idx}>{note}</option>
+                    })
+                }
+            </select>
+            </div>
+            <div className="setting">
+            <label>Scale:</label>
+            <select name="play-scale"
+                onChange={playScaleChange}
+                value={scale}
 
-        >
-            {
-                baseNotes.map((note) => {
-                    return <option>{note}</option>
-                })
-            }
-        </select>
-        
-        <br />
-        <br />
+            >
+                {
+                    Object.keys(harmonicaService.scales).map((s, idx) => {
+                        return <option key={idx}>{s}</option>
+                    })
+                }
+            </select>
+                </div>
+            <div className="setting">
+            <label>Play Key:</label>
+            <select name="play-key"
+                onChange={playKeyChange}
+                value={playKey}
 
+            >
+                {
+                    harmonicaService.baseNotes.map((k, idx) => {
+                        return <option key={idx}>{k}</option>
+                    })
+                }
+            </select>
+                </div>
+        </section>
+        <section id="layout">
+
+       
         <div id='chart'>
 
             {
-                layoutPattern.map((i, idx) => {
-                    console.log('note', notes[i -1], idx)
-                    return <div>
-                        {notes[i - 1]}
+                harmonicaService.layoutPattern.map((i, idx) => {
+                    console.log("THIS IS CURRENT DIV:",notes[i-1], idx, playNotes.includes(notes[i-1]))
+
+                    return <>
+                    {playNotes.includes(notes[i-1]) && <div
+                            className="highlighted"
+                            key={idx}
+                            >
+                            {notes[i - 1]}
                         </div>
+                    }
+
+                        {!playNotes.includes(notes[i-1]) &&<div
+                    key={idx}>
+                        {notes[i - 1]}
+                    </div>
+                    }
+                    </>
                 })
             }
-    
+
         </div>
+        <div id='chart'>
+
+            {
+                harmonicaService.layoutPattern.map((i, idx) => {
+
+                    return <>
+                        {playNotes.includes(notes[i - 1]) && <div
+                            className="highlighted"
+                            key={idx}
+                        >
+                            {notes[i - 1]}
+                        </div>
+                        }
+
+                        {!playNotes.includes(notes[i - 1]) && <div
+                            key={idx}>
+                            {notes[i - 1]}
+                        </div>
+                        }
+                    </>
+                })
+            }
+
+        </div>
+        <div id='chart'>
+
+            {
+                harmonicaService.layoutPattern.map((i, idx) => {
+
+                    return <>
+                        {playNotes.includes(notes[i - 1]) && <div
+                            className="highlighted"
+                            key={idx}
+                        >
+                            {notes[i - 1]}
+                        </div>
+                        }
+
+                        {!playNotes.includes(notes[i - 1]) && <div
+                            key={idx}>
+                            {notes[i - 1]}
+                        </div>
+                        }
+                    </>
+                })
+            }
+
+        </div>
+        </section>
     </>
 }
---
+++ src/utilities/harmonica-service.js
@@ -0,0 +1,62 @@
+
+const baseNotes = [
+    'C', 'C#/D♭', 'D', 'D#/E♭', 'E', 'F', 'F#/G♭', 'G', 'G#A♭', 'A', 'A#B♭', 'B'
+]
+
+const reorderBaseNotesByKey = (key) => {
+    const index = baseNotes.indexOf(key)
+    const part1 = baseNotes.slice(index)
+    const part2 = baseNotes.slice(0, index)
+    const result = part1.concat(part2)
+    console.log('notes in key of:', key, ' ', result);
+    return result
+}
+
+const layoutPattern = [2, 1, 3, 4, 6, 5, 6, 7, 9, 8, 10, 11, 2, 1, 12, 1]
+
+const scales = {
+    'Major Scale': ['whole', 'whole', 'half', 'whole', 'whole', 'whole', 'half'],
+    'Natural Minor Scale': ['whole', 'half', 'whole', 'whole', 'half', 'whole', 'whole',]
+}
+
+const noteSteps = [
+    { note: 'A', whole: 'B', half: 'A#B♭' },
+    { note: 'A#B♭', whole: 'C', half: 'B' },
+    { note: 'B', whole: 'C#/D♭', half: 'C' },
+    { note: 'C', whole: 'D', half: 'C#/D♭' },
+    { note: 'C#/D♭', whole: 'D#/E♭', half: 'D' },
+    { note: 'D', whole: 'E', half: 'D#/E♭' },
+    { note: 'D#/E♭', whole: 'F', half: 'E' },
+    { note: 'E', whole: 'F#/G♭', half: 'F' },
+    { note: 'F', whole: 'G', half: 'F#/G♭' },
+    { note: 'F#/G♭', whole: 'G#A♭', half: 'G' },
+    { note: 'G', whole: 'A', half: 'G#A♭' },
+    { note: 'G#A♭', whole: 'A#B♭', half: 'A' },
+
+]
+
+
+function notesFinder(playKey, scale) {
+
+    let playNotes = [playKey]
+    let currentNote = playKey
+    console.log("We are going to play in key of:",currentNote)
+    for (let i = 0; i < scales[scale].length - 1; i++) {
+        let noteStep = noteSteps.find((n) => { return n.note === currentNote })
+        console.log("Notestep",noteStep)
+        playNotes.push(noteStep[scales[scale][i]])
+        currentNote = noteStep[scales[scale][i]]
+    }
+    console.log(playNotes)
+    return playNotes
+}
+
+
+module.exports = {
+    baseNotes,
+    reorderBaseNotesByKey,
+    layoutPattern,
+    scales, 
+    notesFinder,
+    noteSteps
+}
